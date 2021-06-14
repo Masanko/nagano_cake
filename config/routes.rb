c20e6dd5d903e00bc2e453de8a2f4845b4bee317
Masanko <dwrys431@gmail.com>
2021-06-14 14:54:08 +0000

    ログインページ完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+
+devise_for :admins, controllers: {
+    sessions:      'admin/admins/sessions',
+    passwords:     'admin/admins/passwords',
+    registrations: 'admin/admins/registrations'
+  }
+  devise_for :customers,controllers: {
+    sessions:      'public/customers/sessions',
+    passwords:     'public/customers/passwords',
+    registrations: 'public/customers/registrations'
+  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
